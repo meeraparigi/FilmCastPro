6549e8ecd61772f8642800146dc826913e8ca921
@meeraparigi <meera22_99@yahoo.com>
2025-09-16 22:07:31 +0530

    cleaning up the repo

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "myami" {
-    description = "This is my ami id : "
-    value = data.aws_ami.my_ami.id
-}
-
-output "publicip" {
-    description = "The public ip of my EC2 instance is : "
-    value = aws_instance.my_server.public_ip
-}
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-instance_type = "t2.medium"
-key_filename = "Ubuntu-New-Key"
-key_path = "C:/Users/Meera/Desktop/Ubuntu-New-Key.pem"
